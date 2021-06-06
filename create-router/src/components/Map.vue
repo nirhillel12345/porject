92473b09a89b889644cb874f94095393bbb596d9
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 19:19:34 +0300

    finish day 1

---
+++ create-router/src/components/Map.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="map" style="height: 80vh">
+    <LMap :zoom="zoom" :center="center">
+      <LTileLayer :url="url"></LTileLayer>
+      <LMarker :lat-lng="[40.731810,-73.936542]">
+          <l-icon
+          :icon-anchor="staticAnchor"
+          class-name="someExtraClass"
+          @click="showPopUp()"
+        >
+          <img src="map-service/src/assets/running_man.png">
+        </l-icon>
+      </LMarker>
+      <LMarker :lat-lng="[40.730620,-73.934250]"></LMarker>
+      <LMarker :lat-lng="[40.730529,-73.935949]"></LMarker>
+    </LMap>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { reports } from "../../data/reports_json.json";
+
+export default {
+  name: 'Map',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 16,
+      center: [40.730610,-73.935242],
+      bounds: null,
+      staticAnchor: [16, 37],
+    };
+  },
+  methods: {
+      showPopUp() {
+          
+      }
+  }
+}
+</script>
