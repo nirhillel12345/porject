fd78c3964bff42c0fd20219c58b3a7dd1da2054a
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 19:09:13 +0300

    (feat) update

---
+++ create-router/src/components/form.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="form">
+
+  <div>
+  <div>
+    <md-dialog-confirm
+      :md-active.sync="active"
+      md-title="יצירת התראה"
+      @md-cancel="onCancel"
+      @md-confirm="onConfirm"
+       > 
+          <md-select  v-model="movie" name="eventType" id="eventType">
+          <div v-for="eventType in eventTypes" :key="eventType">
+            <md-option value=eventType>{{eventType}}</md-option>
+            </div>
+          </md-select>
+        
+</md-dialog-confirm>
+    <md-button class="md-primary md-raised" @click="active = true">תחשוב שיש כאן כוונת</md-button>
+    
+  </div>
+  </div>
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'form',
+    data: () => ({
+      eventType: 'godfather',
+      eventTypes: ["f","ssss","sss"],
+      country: null,
+      font: null,
+       active: false,
+      value: null
+    }),
+    methods: {
+      onConfirm () {
+        this.value = 'Agreed'
+      },
+      onCancel () {
+        this.value = 'Disagreed'
+      }
+    }
+  }
+</script>
--- create-router/src/router/index.js
+++ create-router/src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
+import form from '@/components/form.vue'
 Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: home
+    },
+    {
+      path: '/',
+      name: 'form',
+      component: form
     }
   ]
 })
