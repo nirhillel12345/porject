7687cf029cdcfaaf8dc0b772d3c18b87ee412a1d
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 16:18:21 +0300

    feat(map): add the leaflet map component.

--- create-router/src/main.js
+++ create-router/src/main.js
@@ -3,8 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import { Icon } from "leaflet";
+import 'leaflet/dist/leaflet.css';
 
 Vue.config.productionTip = false
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 
 /* eslint-disable no-new */
 new Vue({
